8d546288bcc7da8ddee28afbf89dd824b007c2a3
Marcelo Calarota <mlcalarota@gmail.com>
2016-11-05 02:59:35 -0300

    Actualizo class Algomon con atributos basicos.

--- Algomon/src/Modelo/Algomon.java
+++ Algomon/src/Modelo/Algomon.java
@@ -1,5 +1,9 @@
 package Modelo;
 
-public class Algomon {
+import java.util.Map;
 
+public class Algomon {
+	private String tipo;
+	private int vida;
+	private Map<String, Ataque> ataques; // Ver
 }
