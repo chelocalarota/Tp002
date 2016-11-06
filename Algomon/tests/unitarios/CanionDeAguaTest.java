e3a754b10862a049c707a9c3701941aed779fbf5
Agustin <agushorn145@gmail.com>
2016-11-06 17:32:39 -0300

    Subo el test sin ENIE

---
+++ Algomon/tests/unitarios/CanionDeAguaTest.java
@@ -0,0 +1,31 @@
+package Tests.tests.unitarios;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Modelo.Ataques.CanionDeAgua;
+
+public class CanionDeAguaTest {
+
+	@Test
+	public void test01CanionDeAguaPotenciaOk() {
+		CanionDeAgua ataque = new CanionDeAgua();
+		
+		assertEquals(20, ataque.getPotencia());
+	}
+	
+	@Test
+	public void test02CanionDeAguaCantidadOk() {
+		CanionDeAgua ataque = new CanionDeAgua();
+		
+		assertEquals(8, ataque.getCantidad());
+	}
+	
+	@Test
+	public void test03CañonDeAguaTipoOk() {
+		CanionDeAgua ataque = new CanionDeAgua();
+		
+		assertEquals("A", ataque.getTipo());
+	}
+}
