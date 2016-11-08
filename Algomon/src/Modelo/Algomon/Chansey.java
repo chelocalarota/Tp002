2a44cfff16f80d7ba3769302def0084922e0222a
Matias <fonseca.196@gmail.com>
2016-11-08 20:53:40 -0300

    Modifique Chansey porque no tenia ataque rapido y se confundieron y le habian puesto ataque de burbuja

--- Algomon/src/Modelo/Algomon/Chansey.java
+++ Algomon/src/Modelo/Algomon/Chansey.java
@@ -3,15 +3,14 @@ package Modelo.Algomon;
 import java.util.HashMap;
 import Modelo.TipoNormal;
 import Modelo.Ataques.Ataque;
-import Modelo.Ataques.Burbuja;
+import Modelo.Ataques.AtaqueRapido;
 import Modelo.Ataques.Canto;
 import Modelo.Ataques.LatigoCepa;
 
 public class Chansey extends Algomon{
 	public Chansey(){
-		this.estado = new EstadoNormal();
 		this.ataques = new HashMap<String, Ataque>();
-		this.ataques.put("Burbuja", new Burbuja());
+		this.ataques.put("Ataque Rapido", new AtaqueRapido());
 		this.ataques.put("Latigo Cepa", new LatigoCepa());
 		this.ataques.put("Canto", new Canto());
 		this.vida = 130;
