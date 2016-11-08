b272ddfc73493a6424210144edbe28b3e7b59ddf
Matias <fonseca.196@gmail.com>
2016-11-08 20:54:13 -0300

    agrege casi todos los test hace falta arreglar 1 y hacer el ultimo

--- Algomon/src/tests/unitarios/AtaqueSimplesTest.java
+++ Algomon/src/tests/unitarios/AtaqueSimplesTest.java
@@ -2,6 +2,9 @@ package tests.unitarios;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -93,13 +96,179 @@ public class AtaqueSimplesTest {
 	
 	@Test
 	public void test04BalbasauryChanseyVsSquirtle(){
+		
 		Algomon Squirtle = new Squirtle();
 		Algomon Chansey = new Chansey();
 		Algomon Bulbasaur = new Bulbasaur();
+		
 		double vida = Squirtle.getVida();
+		
 		Squirtle.recibirDanio(Chansey.ataque("Latigo Cepa"));
+		
+		assertEquals((vida-30.0), Squirtle.getVida(),0.001D);
+		
+		vida -= 30.0;
+		
 		Squirtle.recibirDanio(Bulbasaur.ataque("Latigo Cepa"));
-		assertEquals((vida-60), Squirtle.getVida(),0.001D);
+		
+		assertEquals((vida-30.0), Squirtle.getVida(),0.001D);
+	}
+	
+	@Test 
+	public void test05BalbasauryChanseyVsCharmander(){
+		
+		Algomon Charmander = new Charmander();
+		Algomon Bulbasaur = new Bulbasaur();
+		Algomon Chansey = new Chansey();
+		
+		double vida = Charmander.getVida();
+		
+		Charmander.recibirDanio(Chansey.ataque("Latigo Cepa"));
+		
+		assertEquals((vida-7.0), Charmander.getVida(),0.001D);
+		
+		vida -= 7;
+		
+		Charmander.recibirDanio(Bulbasaur.ataque("Latigo Cepa"));
+		
+		assertEquals((vida-7), Charmander.getVida(),0.001D);
+	}
+	
+	
+	@Test
+	public void test06SquirtleVsOtrosAlgomones(){
+		Algomon Bulbasaur = new Bulbasaur();
+		Algomon Chansey = new Chansey();;
+		
+		
+		double vida = Chansey.getVida();
+		
+		Algomon Jigglypuff = new Jigglypuff();
+		vida = Jigglypuff.getVida();
+		
+		
+		Jigglypuff.recibirDanio(Chansey.ataque("Latigo Cepa"));;
+		
+		assertEquals((vida-15), Jigglypuff.getVida(),0.001D);
+		
+		vida -= 15;
+		Jigglypuff.recibirDanio(Bulbasaur.ataque("Latigo Cepa"));;
+		
+		assertEquals((vida-15), Jigglypuff.getVida(),0.001D);
+		
+		Algomon Rattata = new Rattata();
+		vida = Rattata.getVida();
+		
+		
+		
+		Rattata.recibirDanio(Chansey.ataque("Latigo Cepa"));;
+		
+		assertEquals((vida-15), Rattata.getVida(),0.001D);
+		
+		vida -= 15;
+		Rattata.recibirDanio(Bulbasaur.ataque("Latigo Cepa"));;
+		
+		assertEquals((vida-15), Rattata.getVida(),0.001D);
+		
+		
+	}
+	
+	@Test
+	public void test07CharmanderVsBulbasaur(){
+		
+		Algomon Charmander = new Charmander();
+		Algomon Bulbasaur = new Bulbasaur();
+		
+		double vida = Bulbasaur.getVida();
+		
+		Bulbasaur.recibirDanio(Charmander.ataque("Brasas"));
+		
+		assertEquals((vida-32), Bulbasaur.getVida(),0.001D);
+		
+	}
+	
+	@Test
+	public void test08CharmanderVsSquirtle(){
+		
+		Algomon Charmander = new Charmander();
+		Algomon Squirtle = new Squirtle();
+		
+		double vida = Squirtle.getVida();
+		
+		Squirtle.recibirDanio(Charmander.ataque("Brasas"));
+		
+		assertEquals((vida-8), Squirtle.getVida(),0.001D);
+		
+	}
+	
+	@Test
+	public void test09CharmanderVsOtrosAlgomones(){
+		Algomon Charmander = new Charmander();;
+		Algomon Chansey = new Chansey();
+		double vida = Chansey.getVida();
+		
+		
+		Chansey.recibirDanio(Charmander.ataque("Brasas"));
+		
+		assertEquals((vida-16), Chansey.getVida(),0.001D);
+
+		Algomon Jigglypuff = new Jigglypuff();
+		vida = Jigglypuff.getVida();
+		
+		
+		Jigglypuff.recibirDanio(Charmander.ataque("Brasas"));
+		
+		assertEquals((vida-16), Jigglypuff.getVida(),0.001D);
+		
+		Algomon Rattata = new Rattata();
+		vida = Rattata.getVida();
+		
+		
+		Rattata.recibirDanio(Charmander.ataque("Brasas"));
+		
+		assertEquals((vida-16), Rattata.getVida(),0.001D);
+	
+		
+	}
+	@Test
+	public void test10AlgomonesAtacanConAtaqueRapido(){
+		List<Algomon> TodosLosAlgomones = new ArrayList<Algomon>();
+		Algomon Jigglypuff = new Jigglypuff();
+		Algomon Rattata = new Rattata();
+		Algomon Chansey = new Chansey();
+		
+		TodosLosAlgomones.add(new Chansey());
+		TodosLosAlgomones.add(new Rattata());
+		TodosLosAlgomones.add(new Bulbasaur());
+		TodosLosAlgomones.add(new Charmander());
+		TodosLosAlgomones.add(new Squirtle());
+		TodosLosAlgomones.add(Rattata);
+		TodosLosAlgomones.add(Chansey);
+		TodosLosAlgomones.add(Jigglypuff);
+		
+		for(Algomon atacado : TodosLosAlgomones){
+			double vida = atacado.getVida();
+			atacado.recibirDanio(Rattata.ataque("Ataque Rapido"));
+			
+			assertEquals((vida-10), atacado.getVida(),0.001D);
+			
+			vida -= 10;
+			
+			atacado.recibirDanio(Jigglypuff.ataque("Ataque Rapido"));
+
+			assertEquals((vida-10), atacado.getVida(),0.001D);
+			
+			vida -= 10;
+			
+			atacado.recibirDanio(Chansey.ataque("Ataque Rapido"));
+		
+			assertEquals((vida-10), atacado.getVida(),0.001D);
+	
+		}
+	}
+	@Test
+	public void test11AlgomonesAgotanCantidadDeAtaques(){
+		
 	}
 
 }
