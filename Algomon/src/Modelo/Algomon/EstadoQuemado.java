3f8b836e61bf9b23c10a942c367faed9cf9ac2c4
yuquimura <brianzambellitello@gmail.com>
2016-11-09 15:32:03 -0300

    Correccion en estadoQuemado

--- Algomon/src/Modelo/Algomon/EstadoQuemado.java
+++ Algomon/src/Modelo/Algomon/EstadoQuemado.java
@@ -5,7 +5,7 @@ public class EstadoQuemado implements Estado {
 	@Override
 	public boolean getEstado(Algomon unAlgomon) {
 		unAlgomon.cambiarVida((int) (-unAlgomon.getVida()*0.1));
-		return true;
+		return false;
 	}
 
 }
