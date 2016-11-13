71f34a89f9f79db460d5582a47f264db8c8bb6b4
Agustin <agushorn145@gmail.com>
2016-11-13 17:28:42 -0300

    Aca hay un Hola Mundo en java fx

---
+++ HolaMundo/src/com/ejemplo/holamundo/HolaMundo.java
@@ -0,0 +1,14 @@
+package com.ejemplo.holamundo;
+
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo :)");
+
+	}
+
+}
