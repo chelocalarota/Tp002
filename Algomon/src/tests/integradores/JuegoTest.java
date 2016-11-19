79109d5f5424805423a188cfb82020c2f9d1f872
Marcelo Calarota <mlcalarota@gmail.com>
2016-11-19 16:37:19 -0300

    JuegoTest: *mas tests

--- Algomon/src/tests/integradores/JuegoTest.java
+++ Algomon/src/tests/integradores/JuegoTest.java
@@ -5,23 +5,110 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import modelo.Juego;
+import modelo.Jugador;
 import modelo.VictoriaObtenidaException;
 import modelo.algomon.*;
 import modelo.enums.AtaquesEnum;
+import modelo.tipos.Tipo;
 
 public class JuegoTest {
 
 	@Test
-	public void AtaqueBasico() throws SinPuntosDePoderException, EstaDormidoException, PokemonMuertoException, VictoriaObtenidaException{
+	public void test01ResolverAtaqueCuandoAlgomonAtaca() throws SinPuntosDePoderException, EstaDormidoException, PokemonMuertoException, VictoriaObtenidaException{
 		Juego juego = new Juego();
 		juego.agregarCharmander();
 		juego.elegirPokemonInicial(0);
 		juego.cambiarJugador();
 		juego.agregarSquirtle();
 		juego.elegirPokemonInicial(0);
-		juego.JugarTurnoAtaque(AtaquesEnum.BURBUJA);
-		assertEquals(juego.obtenerJugadorDefensor().getPokemonActivo().getVida(),150);
+		
+		int vidaAlgomonDefensor = juego.obtenerJugadorDefensor().getPokemonActivo().getVida();
+		int valorAtaque = (int)juego.obtenerJugadorActual().getPokemonActivo().ataque(AtaquesEnum.BURBUJA).getPotencia();
+		int multiplicador = (int)juego.obtenerJugadorActual().getPokemonActivo().ataque(AtaquesEnum.BURBUJA).getTipo().respuestaAFuego();
+		vidaAlgomonDefensor = vidaAlgomonDefensor - (valorAtaque * multiplicador);
+		
+		juego.resolverAtaque(AtaquesEnum.BURBUJA);
+		assertEquals(juego.obtenerJugadorDefensor().getPokemonActivo().getVida(), vidaAlgomonDefensor);
 	}
+	
+	@Test
+	public void test02AgregarAlgomonAJugador(){
+		Juego juego = new Juego();
+		Bulbasaur bulbasaur = new Bulbasaur();
+		juego.agregarBulbasaur();
+		Algomon algomon = juego.obtenerJugadorActual().getPokemonActivo();
+		
+		assertEquals(algomon.getClass(), bulbasaur.getClass());
+	}
+	
+	@Test
+	public void test03AgregarAlgomonAJugador(){
+		Juego juego = new Juego();
+		Charmander charmander = new Charmander();
+		juego.agregarCharmander();
+		Algomon algomon = juego.obtenerJugadorActual().getPokemonActivo();
+		
+		assertEquals(algomon.getClass(), charmander.getClass());
+	}
+	
+	@Test
+	public void test04AgregarAlgomonAJugador(){
+		Juego juego = new Juego();
+		Chansey chansey = new Chansey();
+		juego.agregarChansey();
+		Algomon algomon = juego.obtenerJugadorActual().getPokemonActivo();
+		
+		assertEquals(algomon.getClass(), chansey.getClass());
+	}
+	
+	@Test
+	public void test05AgregarAlgomonAJugador(){
+		Juego juego = new Juego();
+		Jigglypuff jiggly = new Jigglypuff();
+		juego.agregarJigglypuff();
+		Algomon algomon = juego.obtenerJugadorActual().getPokemonActivo();
+		
+		assertEquals(algomon.getClass(), jiggly.getClass());
+	}
+	
+	@Test
+	public void test06AgregarAlgomonAJugador(){
+		Juego juego = new Juego();
+		Rattata rata = new Rattata();
+		juego.agregarRattata();
+		Algomon algomon = juego.obtenerJugadorActual().getPokemonActivo();
+		
+		assertEquals(algomon.getClass(), rata.getClass());
+	}
+	
+	@Test
+	public void test07AgregarAlgomonAJugador(){
+		Juego juego = new Juego();
+		Squirtle tortuga = new Squirtle();
+		juego.agregarSquirtle();
+		Algomon algomon = juego.obtenerJugadorActual().getPokemonActivo();
+		
+		assertEquals(algomon.getClass(), tortuga.getClass());
+	}
+	
+	@Test
+	public void test08VerificarJugadorActivoYCambiarJugador(){
+		Juego juego = new Juego();
+		Squirtle tortuga = new Squirtle();
+		Rattata rata = new Rattata();
+		
+		juego.agregarSquirtle();
+		Algomon algomon1 = juego.obtenerJugadorActual().getPokemonActivo();
+		juego.cambiarJugador();
+		juego.agregarRattata();
+		Algomon algomon2 = juego.obtenerJugadorActual().getPokemonActivo();
+		
+		
+		assertEquals(algomon1.getClass(), tortuga.getClass());
+		assertEquals(algomon2.getClass(), rata.getClass());
+		assertNotEquals(algomon1.getClass(), algomon2.getClass());
+	}
+	
 	@Test
 	public void unJugadorGana() throws SinPuntosDePoderException, EstaDormidoException, PokemonMuertoException{
 		Juego juego = new Juego();
@@ -46,7 +133,7 @@ public class JuegoTest {
 		while (true){
 
 			try {
-				juego.JugarTurnoAtaque(AtaquesEnum.BURBUJA);
+				juego.resolverAtaque(AtaquesEnum.BURBUJA);
 				juego.pasarTurno();
 			}
 		 catch (VictoriaObtenidaException e) {
