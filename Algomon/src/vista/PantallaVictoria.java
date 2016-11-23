0828890513f4768261f1de42a569d60f0a459dff
yuquimura <brianzambellitello@gmail.com>
2016-11-23 14:09:50 -0300

    jejeje perdon mati

--- Algomon/src/vista/PantallaVictoria.java
+++ Algomon/src/vista/PantallaVictoria.java
@@ -1,6 +1,7 @@
 package vista;
 
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 
 import javafx.stage.Stage;
@@ -9,5 +10,7 @@ public class PantallaVictoria {
 	public void cargarPantalla(Stage stage, ControladorLogicoDelJuego controlado){
 		BorderPane border = new BorderPane();
 		Scene principal = new Scene(border, 981, 600);
+		CreadorImagen creadorImagen = new CreadorImagen();
+		ImageView imageViewTitulo = creadorImagen.crearImageView("/vista/imagenes/algomon.png");
 	}
 }
