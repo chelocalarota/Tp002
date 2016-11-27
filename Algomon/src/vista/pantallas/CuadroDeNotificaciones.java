4be71b83fd175700bdd75e6a70e9aca909622846
Agustin <agushorn145@gmail.com>
2016-11-27 12:35:26 -0300

    Puse el label para notificar a los usuarios sobre el click derecho.

--- Algomon/src/vista/pantallas/CuadroDeNotificaciones.java
+++ Algomon/src/vista/pantallas/CuadroDeNotificaciones.java
@@ -9,45 +9,32 @@ public class CuadroDeNotificaciones {
 
 	public CuadroDeNotificaciones(){
 		this.textArea = new TextArea();
+		this.textArea.setPrefColumnCount(10);
+		this.textArea.setPrefRowCount(5);
 		this.textArea.setEditable(false);
 		this.textArea.setWrapText(true);
+		this.textArea.setCenterShape(true);
+		this.textArea.setStyle(" -fx-font-family: arial; -fx-font-size: 26;-fx-text-alignment: center;-fx-padding: 50 50 50 50");
 	}
 
 	public TextArea getTextArea(){
 		return this.textArea;
 	}
 
-	public void notificarCambioDeTurno(String jugador){
-		this.textArea.setText(jugador + "ha terminado su turno.");
-	}
-
-	public void notificarAtaque(String pokemon, String ataque){
-		this.textArea.setText(pokemon + "ha realizado" + ataque + ".");
-	}
-
-	public void notificarDanio(String pokemon, String vida){
-		this.textArea.setText(pokemon + "ha perdido" + vida + "puntos de vida.");
-	}
-
-	public void notificarUsoDeItem(ItemsEnum item, String jugador){
-		this.textArea.setText(jugador + "ha utilizado" + item);
+	public void notificarUsoDeItem(ItemsEnum item){
+		this.textArea.setText("Se ha utilizado " + item);
 	}
 
 	public void notificarNoHayItemDisponible(ItemsEnum item){
 		this.textArea.setText("No hay mas " + item + " disponible.\n");
 	}
 
-	public void notificarCambioDePokemon(String jugador, String pokemon){
-		this.textArea.setText("El" + jugador + "ha cambiado de pokemon." + pokemon + "salió al combate.");
-	}
-
-	public void notificarPokemonMuerto(){
+		public void notificarPokemonMuerto(){
 		this.textArea.setText("Este pokemon ha muerto. No podrás seguir utilizándolo.");
 	}
 
 	public void notificar(String string){
 		this.textArea.setText(string);
-		textArea.setEditable(true);
-		textArea.setStyle("-fx-font: 34 arial; -fx-base: #b6e7c9;");
+
 	}
 }
